28faa1215bca16b627d286e58d8fe3e79b9250f7
raymond <pc8521@gmail.com>
2025-09-23 11:37:01 +0800

    add about page

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Hello World!</h1>")
+
+def about(request):
+    return render(request, 'pages/about.html', {})
+
