e8c4015488f995848980fe866233ebd211e65e51
raymond <pc8521@gmail.com>
2025-09-30 10:48:26 +0800

    change admin page style

--- config/urls.py
+++ config/urls.py
@@ -17,12 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include("pages.urls", namespace="pages")),
     path('listings/',include("listings.urls", namespace="listings")),
     path('admin/', admin.site.urls),
-] + debug_toolbar_urls()
+] + debug_toolbar_urls() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "ERB7 Medical Center Admin Portal"
 admin.site.site_title = "ERB7 Medical Center Admin Portal"
