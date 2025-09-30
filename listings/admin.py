3b89ac6605c5bf7d7a1549b3b55d25677037b047
raymond <pc8521@gmail.com>
2025-09-30 12:12:08 +0800

    auto update

--- listings/admin.py
+++ listings/admin.py
@@ -3,4 +3,15 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Listing
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'district', 'is_published', 'doctor'
+    list_display_links = 'id', 'title'
+    list_filter = 'doctor',
+    list_editable = 'is_published',
+    search_fields = 'title', 'district', 'doctor'
+    list_per_page = 25
+    ordering = ['-id']
+    prepopulated_fields = {'title': ('title',)}
+    show_facets=admin.ShowFacets.ALWAYS
+    
+admin.site.register(Listing, ListingAdmin)
