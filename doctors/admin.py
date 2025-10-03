eb9e255fb3de80187dcd81c4f5d455789492adda
raymond <pc8521@gmail.com>
2025-10-03 17:31:21 +0800

    auto update

--- doctors/admin.py
+++ doctors/admin.py
@@ -4,9 +4,9 @@ from .models import Doctor
 # Register your models here.
 
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'is_mvp', 'hire_date')
+    list_display = ('name', 'email', 'is_mvp', 'hire_date', 'phone')
     list_display_links=('name', 'email')
-    list_editable = ('is_mvp',)
+    list_editable = ('is_mvp','phone')
     search_fields = ('name',)
     list_per_page = 25
 
