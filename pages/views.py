d1db76ee448c1e9a07a815d99763e86bec57acf5
raymond <pc8521@gmail.com>
2025-10-03 16:16:54 +0800

    auto update

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
+from doctors.models import Doctor
 
 # Create your views here.
 
@@ -13,7 +14,10 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    doctors = Doctor.objects.order_by('-hire_date')[:3] # Get the 3 most recent doctors
+    mvp_doctor = Doctor.objects.all().filter(is_mvp=True) # Get the MVP doctor
+    context={'doctors': doctors, 'mvp_doctor': mvp_doctor}
+    return render(request, 'pages/about.html', context)
 
 def test(request):
     # print(request.path)
