349bdd655ef55a62507c97bea4785b90c0fa9a15
raymond <pc8521@gmail.com>
2025-10-13 17:03:40 +0800

    auto update

--- accounts/views.py
+++ accounts/views.py
@@ -44,6 +44,8 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
     return redirect('pages:index')
 
 def dashboard(request):
