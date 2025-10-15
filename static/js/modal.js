6ce55b558ed06dac2538c1f2a471fc8c6f879e20
raymond <pc8521@gmail.com>
2025-10-15 09:36:58 +0800

    auto update

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
     // when the modal is shown
-    $("#deleteConfirmationModal").on('show.bs.modal', function (event) {
+    $("#deleteConfirmModal").on('show.bs.modal', function (event) {
         const button = $(event.relatedTarget); // Button that triggered the modal
         const contactId = button.data('id'); // Extract info from data-* attributes
         const deleteUrl = button.data('url'); // Extract delete URL
         $("#modal-contact-id").text(contactId);
         // set the delete URL
-        $("#delete-contact-form").attr('href', deleteUrl);
+        $("#confirmDeleteBtn").attr('href', deleteUrl);
     });
     // when the delete button is clicked, submit the form
-    $("#confirm-delete-btn").click(function(event){
+    $("#confirmDeleteBtn").click(function(event){
         // close the modal
-        $("#deleteConfirmationModal").modal('hide');
+        $("#deleteConfirmModal").modal('hide');
         setTimeout(() => {
-            window.location.href = $("#delete-contact-form").attr('href');
+            window.location.href = $("#confirmDeleteBtn").attr('href');
         },300);
     });
 });
